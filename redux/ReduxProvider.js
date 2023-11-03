0b87c954a9b6617d5966a711a27af06b48b5f8a9
Gautam Raj <ashragautam25@gmail.com>
2023-11-03 21:23:27 +0530

    add images, loading animation and redux provider

---
+++ redux/ReduxProvider.js
@@ -0,0 +1,10 @@
+"use client";
+
+import { store } from "./store.js";
+import { Provider } from "react-redux";
+
+export const ReduxProvider = ({ children }) => {
+  return <Provider store={store}>{children}</Provider>;
+};
+
+export default ReduxProvider;
