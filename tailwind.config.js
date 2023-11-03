74bae24b68893080d6f76c0bac21b415c3fcdc93
Gautam Raj <ashragautam25@gmail.com>
2023-11-03 21:03:39 +0530

    add styles and packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require('@material-tailwind/react/utils/withMT');
+module.exports = withMT({
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -9,10 +10,31 @@ module.exports = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+
+        'gradient-linear':
+          'linear-gradient(270deg, #1C1D22 0%, #1C1D22 50%, #F0F0F0 50%, #F0F0F0 100%)',
+
+        'gradient-priamry': 'linear-gradient(225deg, #49E9FF 0%, #40FF8D 100%)',
+      },
+
+      boxShadow: {
+        'primary-form-black': '0px 4px 36px 36px rgba(0, 0, 0, 0.25)',
+      },
+
+      colors: {
+        'primary-black': '#1C1D22',
+        'primary-white': '#F0F0F0',
+        primary: '#6CCBB9',
+      },
+      typography: {
+        button: {
+          textTransform: 'none',
+        },
       },
     },
   },
   plugins: [],
-}
+});
